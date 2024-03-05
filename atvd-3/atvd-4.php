f2f27995d35c5312ca7e905339a8ec96cd8b8dbe
CaioTico <caiooprogramador8@gmail.com>
2024-03-05 12:03:19 -0300

    código comentado

--- atvd-3/atvd-4.php
+++ atvd-3/atvd-4.php
@@ -4,18 +4,36 @@
     <title>Informações sobre o Time</title>
 </head>
 <body>
- 
-<?php
+
+ <?
+// Define o nome da equipe
 $nomeTime = "Milan";
-$ano = 1995;
+
+// Define o ano em que a equipe foi estabelecida
 $anoTime = 1899;
+
+// Define o ano atual
+$ano = 1995;
+
+// Armazena a primeira frase sobre a história da equipe
 $frase1 = "O Milan foi maior campeão Mundial da história.";
-$frase2 = "Em $ano, ele recebeu o titulo de '1º Melhor Clube do Mundo'.";
- 
+
+// Armazena a segunda frase sobre a história da equipe
+$frase2 = "Em $ano, ele recebeu o título de '1º Melhor Clube do Mundo'.";
+
+// Exibe o título principal
 echo "<h1>Informações sobre o Time</h1>";
+
+// Exibe o nome da equipe
 echo "<p>Nome do time: $nomeTime</p>";
+
+// Exibe o ano em que a equipe foi estabelecida
 echo "<p>Ano: $anoTime</p>";
+
+// Exibe a primeira frase sobre a história da equipe
 echo "<p> $frase1</p>";
+
+// Exibe a segunda frase sobre a história da equipe
 echo "<p> $frase2</p>";
 ?>
  
