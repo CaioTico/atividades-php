1a910fabb9e55251457cfc7a1329752f9dfd945b
CaioTico <caiooprogramador8@gmail.com>
2024-04-01 09:15:09 -0300

    atualizados

--- atvd-9/calcular_salario.php
+++ atvd-9/calcular_salario.php
@@ -53,7 +53,7 @@ echo "<!DOCTYPE html>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Resultado do Cálculo</title>
-    <link rel='stylesheet' href='styles.css'>
+    <link rel='stylesheet' href='style.css'>
 </head>
 <body>
     <div class='result'>
